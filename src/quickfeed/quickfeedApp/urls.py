a7d1abf3f1a6b4e0cbfc1e05abeea715f3fcb3c9
akshaysen08 <akshaysen.sen75@gmail.com>
2022-04-18 09:08:23 -0400

    Added Login and Register pages

--- src/quickfeed/quickfeedApp/urls.py
+++ src/quickfeed/quickfeedApp/urls.py
@@ -4,7 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #url  configuration
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('login', views.login, name='login-page'),
+    path('register', views.register, name='register-page'),
+    path('old', views.homepageold, name='oldhome')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- src/quickfeed/quickfeedApp/views.py
+++ src/quickfeed/quickfeedApp/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 
 def homepage(request):
         return render(request, 'index.html', {'title': 'Welcome to quickfeed'})
+
+def login(request):
+        return render(request, 'login.html', {'title': 'Login - Quickfeed'})
+
+def register(request):
+        return render(request, 'register.html', {'title': 'Register - Quickfeed'})
+
+def homepageold(request):
+        return render(request, 'homepage.html', {'title': 'OLD Welcome to quickfeed'})
