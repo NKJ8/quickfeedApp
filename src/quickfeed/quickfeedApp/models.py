73c7f6c68bfaf846fadc30a53e8b53acde8b3441
NKJ8 <niteshkjoshi@gmnail.com>
2022-04-18 23:27:59 -0400

    'registration'

--- src/quickfeed/quickfeedApp/models.py
+++ src/quickfeed/quickfeedApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    validity = models.CharField(max_length=100)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+class Subscription(models.Model):
+    plan_id = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.SET_NULL, null=True)
+    from_date = models.DateTimeField()
+    to_date = models.DateTimeField()
+   
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    dob = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    review_count = models.CharField(max_length=100)
+    subscription_id = models.ForeignKey(Subscription, related_name='users', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        User.objects.create(user=instance)
+    instance.profile.save()
--- src/quickfeed/quickfeedApp/views.py
+++ src/quickfeed/quickfeedApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 from django.http import HttpResponse
 # Create your views here.
 # request handler
@@ -6,3 +8,31 @@ from django.http import HttpResponse
 
 def homepage(request):
         return render(request, 'index.html', {'title': 'Welcome to quickfeed'})
+
+def login(request):
+       return render(request, 'login.html', {'title': 'Login - Quickfeed'})
+
+def register(request):
+       return render(request, 'register.html', {'title': 'Register - Quickfeed'})
+
+def homepageold(request):
+        return render(request, 'homepage.html', {'title': 'OLD Welcome to quickfeed'})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.dob = form.cleaned_data.get('dob')
+            user.name = form.cleaned_data.get('name')
+            user.address = form.cleaned_data.get('address')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
