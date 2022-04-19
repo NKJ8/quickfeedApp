dd98db1c3f8a944f26e5ebaf5645d6763b406d7f
akshaysen08 <akshaysen.sen75@gmail.com>
2022-04-18 20:45:28 -0400

    MOdels and Migrations created

--- src/quickfeed/quickfeedApp/models.py
+++ src/quickfeed/quickfeedApp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    validity = models.CharField(max_length=100)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+class Subscription(models.Model):
+    plan_id = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.SET_NULL, null=True)
+    from_date = models.DateTimeField()
+    to_date = models.DateTimeField()
+   
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    dob = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    review_count = models.CharField(max_length=100)
+    subscription_id = models.ForeignKey(Subscription, related_name='users', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    dob = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    review_count = models.CharField(max_length=100)
+    subscription_id = models.ForeignKey(Subscription, related_name='users', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+class CardDetails(models.Model):
+    card_number = models.CharField(max_length=20)
+    card_exp_month = models.CharField(max_length=2)
+    card_exp_year = models.CharField(max_length=4)
+    user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
+
+class Reviews(models.Model):
+    review = models.CharField(max_length=20)
+    user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
+
+
+
+
