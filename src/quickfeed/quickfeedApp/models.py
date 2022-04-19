219b0959e771e263822a327c17db742ffdbc623c
akshaysen08 <akshaysen.sen75@gmail.com>
2022-04-19 17:18:07 -0400

    Models and migrations

--- src/quickfeed/quickfeedApp/models.py
+++ src/quickfeed/quickfeedApp/models.py
@@ -1,3 +1,5 @@
+
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -33,8 +35,9 @@ class Business(models.Model):
     phone = models.CharField(max_length=100)
     dob = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
     review_count = models.CharField(max_length=100)
-    subscription_id = models.ForeignKey(Subscription, related_name='users', on_delete=models.SET_NULL, null=True)
+    subscription_id = models.ForeignKey(Subscription, related_name='business', on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -43,12 +46,22 @@ class CardDetails(models.Model):
     card_number = models.CharField(max_length=20)
     card_exp_month = models.CharField(max_length=2)
     card_exp_year = models.CharField(max_length=4)
-    user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, related_name='card_details', on_delete=models.SET_NULL, null=True)
 
 class Reviews(models.Model):
     review = models.CharField(max_length=20)
     user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
-    user_id = models.ForeignKey(User, related_name='users', on_delete=models.SET_NULL, null=True)
+    business_id = models.ForeignKey(Business, related_name='reviews', on_delete=models.SET_NULL, null=True)
+    anonymous = models.BooleanField(default=False)
+    date = models.DateTimeField()
+
+class Profile(models.Model):
+    business_id = models.ForeignKey(Business, related_name='profile', on_delete=models.SET_NULL, null=True)
+    total_reviews = models.CharField(max_length=20)
+    total_like = models.CharField(max_length=20)
+    total_ratings = models.CharField(max_length=20)
+    total_customers = models.CharField(max_length=20)
+
 
 
 
