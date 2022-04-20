2062221eae6fd92158d816ef276dbe6c02db7f9a
NKJ8 <niteshkjoshi@gmnail.com>
2022-04-20 19:53:26 -0400

    'forms'

---
+++ src/quickfeed/quickfeedApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    dob = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+    address = forms.CharField(help_text='Enter address')
+    name = forms.CharField(help_text='Enter name')
+    phone = forms.CharField(help_text='Enter phone')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','name','phone','dob','address', 'password1', 'password2')
